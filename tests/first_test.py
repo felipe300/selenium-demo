3a9af129427c7a001c8321734d9d88c74b62cd43
Felipe Gutiérrez <caracoloro@gmail.com>
2025-07-17 21:51:50 -0400

    bug: fix yml file comments

--- tests/first_test.py
+++ tests/first_test.py
@@ -3,8 +3,8 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.common.by import By
 
 def test_page_title():
-    # browser = webdriver.Firefox(service=FirefoxService("/usr/local/bin/geckodriver"))
-    browser = webdriver.Firefox()
+    browser = webdriver.Firefox(service=FirefoxService("/usr/local/bin/geckodriver"))
+    # browser = webdriver.Firefox()
     browser.get("https://github.com")
 
     titleElement = browser.find_element(By.ID, "hero-section-brand-heading")
