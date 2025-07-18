b5065e595727b41b4f592a9cf117a97d799fb6eb
Felipe Gutiérrez <caracoloro@gmail.com>
2025-07-17 22:24:21 -0400

    bug: fix test with headless browser

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,10 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
 
 def test_page_title():
+    # Configure Firefox to run in headless mode
+    firefox_options = FirefoxOptions()
+    firefox_options.add_argument("--headless") # This is the crucial line
+    firefox_options.add_argument("--no-sandbox")
+    firefox_options.add_argument("--disable-dev-shm-usage")
+
+    # Initialize Firefox with the configured options
+    browser = webdriver.Firefox(options=firefox_options)
+
     # browser = webdriver.Firefox(service=FirefoxService("/usr/local/bin/geckodriver"))
-    browser = webdriver.Firefox()
+    # browser = webdriver.Firefox()
     browser.get("https://github.com")
 
     titleElement = browser.find_element(By.ID, "hero-section-brand-heading")
